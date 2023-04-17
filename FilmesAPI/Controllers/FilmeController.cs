8f16c5dded62796dc9edf99a71cfa695267af310
Patrick Neves <you@example.com>
2023-04-17 20:08:43 -0300

    Aulas de C#

---
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,49 @@
+﻿using AutoMapper;
+using FilmesAPI.Data;
+using FilmesAPI.Data.DTO;
+using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FilmeController : ControllerBase
+{
+
+    private FilmeContext _context;
+    private IMapper _mapper;
+
+    public FilmeController(FilmeContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+
+    [HttpPost]
+    public IActionResult AdicionaFilme([FromBody] CreateFilmeDto filmeDto)
+    {
+        FilmeViewModel filme = _mapper.Map<FilmeViewModel>(filmeDto);
+        _context.Filme.Add(filme);
+        _context.SaveChanges();
+        return CreatedAtAction(nameof(RecuperaFilme), new {id = filme.Id}, filmeDto);
+    }
+
+    [HttpGet]
+    public IEnumerable<FilmeViewModel> RecuperaFilme([FromQuery] int skip = 0, 
+        [FromQuery] int take = 50)
+    {
+        return _context.Filme.Skip(skip).Take(take);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult RecuperaFilme(int id)
+    {
+        var filme = _context.Filme.FirstOrDefault(filme => filme.Id == id);
+        if (filme == null)  return NotFound();
+
+        return Ok(filme);
+
+    }
+}
+ 
---
+++ FilmesAPI/Models/FilmeViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models;
+
+public class FilmeViewModel
+{
+    [Key]
+    [Required]
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "O Título do filme é obrigatório")]
+    public string Titulo { get; set; }
+
+    [Required(ErrorMessage = "O Gênero do filme é obrigatório")]
+    [MaxLength(50, ErrorMessage ="O tamanho do Gênero não pode exceder 50 caracteres")]
+    public string Genero { get; set; }
+
+    [Required(ErrorMessage = "A Duração do filme é obrigatório")]
+    [Range(70, 600, ErrorMessage = "A duração do filme não pode ser superior a 600 minutos")]
+    public int Duracao { get; set; }
+
+    [Required(ErrorMessage = "O Diretor do filme é obrigatório")]
+    public string Diretor { get; set; }
+
+    public int AnoLancamento { get; set; }
+}
